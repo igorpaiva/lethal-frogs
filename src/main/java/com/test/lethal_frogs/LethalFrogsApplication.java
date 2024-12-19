69c93343ae5e6493c378c16049d453416abc2311
Igor Paiva <igor.araujo.ee@gmail.com>
2024-12-19 23:17:55 +0100

    First commit

---
+++ src/main/java/com/test/lethal_frogs/LethalFrogsApplication.java
@@ -0,0 +1,13 @@
+package com.test.lethal_frogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LethalFrogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LethalFrogsApplication.class, args);
+	}
+
+}
